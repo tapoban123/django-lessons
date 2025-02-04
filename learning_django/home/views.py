d2799659641313a9de51d4f1b565626be9e9331a
Tapoban Ray <raytapoban@gmail.com>
2025-02-05 01:46:10 +0530

    Learnt about views, templates and urls

--- learning_django/home/views.py
+++ learning_django/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, "index.html")
+
+def successPage(request):
+    return HttpResponse("<h1>Hello, This is Success Page.</h1>")
--- learning_django/learning_django/urls.py
+++ learning_django/learning_django/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from home.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", home, name="home"),
+    path("success-page/", successPage, name="success"),
+    path("admin/", admin.site.urls),
 ]
