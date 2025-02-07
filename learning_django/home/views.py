e15a73e785662e9251cde57c73cd596635336927
Tapoban Ray <raytapoban@gmail.com>
2025-02-08 01:08:10 +0530

    Learnt about Django Template tags

--- learning_django/home/views.py
+++ learning_django/home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    people_data = [
+        {"name": "Arun Singh", "age": 19},
+        {"name": "Meena", "age": 16},
+        {"name": "Hari", "age": 28},
+        {"name": "Mikey", "age": 14},
+        {"name": "Andrews", "age": 30},
+    ]
+    return render(request, "index.html", context={"peoples": people_data})
+
 
 def successPage(request):
     return HttpResponse("<h1>Hello, This is Success Page.</h1>")
